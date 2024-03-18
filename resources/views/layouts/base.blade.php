effbd47f483994f66e042413ecd403a73a6f0a51
AlmeidaManzolo <mcompsnetwork@gmail.com>
2024-03-18 09:31:38 +0300

    layout setup

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Booksto - Responsive Bootstrap 4 Admin Dashboard Template</title>
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" />
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+      <!-- Typography CSS -->
+      <link rel="stylesheet" href="{{ asset('css/typography.css') }}">
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+      @livewireStyles
+   </head>
+   {{ $slot }}
+
+   </body>
+      <!-- Wrapper END -->
+      <!-- Footer -->
+      <footer class="iq-footer">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-lg-6">
+                  <ul class="list-inline mb-0">
+                     <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
+                     <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                  </ul>
+               </div>
+               <div class="col-lg-6 text-right">
+                  Copyright <span id="copyright">
+<script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script>
+</span> <a href="#">Booksto</a> All Rights Reserved.
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- Footer END -->
+      <!-- color-customizer -->
+      <div class="iq-colorbox color-fix">
+         <div class="buy-button"> <a class="color-full" href="#"><i class="fa fa-spinner fa-spin"></i></a> </div>
+         <div id="right-sidebar-scrollbar" class="iq-colorbox-inner">
+            <div class="clearfix color-picker">
+               <h3 class="iq-font-black">Booksto Awesome Color</h3>
+               <p>This color combo available inside whole template. You can change on your wish, Even you can create your own with limitless possibilities! </p>
+               <ul class="iq-colorselect clearfix">
+                  <li class="color-1 iq-colormark" data-style="color-1"></li>
+                  <li class="color-2" data-style="iq-color-2"></li>
+                  <li class="color-3" data-style="iq-color-3"></li>
+                  <li class="color-4" data-style="iq-color-4"></li>
+                  <li class="color-5" data-style="iq-color-5"></li>
+                  <li class="color-6" data-style="iq-color-6"></li>
+                  <li class="color-7" data-style="iq-color-7"></li>
+                  <li class="color-8" data-style="iq-color-8"></li>
+                  <li class="color-9" data-style="iq-color-9"></li>
+                  <li class="color-10" data-style="iq-color-10"></li>
+                  <li class="color-11" data-style="iq-color-11"></li>
+                  <li class="color-12" data-style="iq-color-12"></li>
+                  <li class="color-13" data-style="iq-color-13"></li>
+                  <li class="color-14" data-style="iq-color-14"></li>
+                  <li class="color-15" data-style="iq-color-15"></li>
+                  <li class="color-16" data-style="iq-color-16"></li>
+                  <li class="color-17" data-style="iq-color-17"></li>
+                  <li class="color-18" data-style="iq-color-18"></li>
+                  <li class="color-19" data-style="iq-color-19"></li>
+                  <li class="color-20" data-style="iq-color-20"></li>
+               </ul>
+               <a target="_blank" class="btn btn-primary d-block mt-3" href="#">Purchase Now</a>
+            </div>
+         </div>
+      </div>
+      <!-- color-customizer END -->
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="{{ asset('js/jquery.min.js') }}"></script>
+      <script src="{{ asset('js/popper.min.js') }}"></script>
+      <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+      <!-- Appear JavaScript -->
+      <script src="{{ asset('js/jquery.appear.js') }}"></script>
+      <!-- Countdown JavaScript -->
+      <script src="{{ asset('js/countdown.min.js') }}"></script>
+      <!-- Counterup JavaScript -->
+      <script src="{{ asset('js/waypoints.min.js') }}"></script>
+      <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+      <!-- Wow JavaScript -->
+      <script src="{{ asset('js/wow.min.js') }}"></script>
+      <!-- Apexcharts JavaScript -->
+      <script src="{{ asset('js/apexcharts.js') }}"></script>
+      <!-- Slick JavaScript -->
+      <script src="{{ asset('js/slick.min.js') }}"></script>
+      <!-- Select2 JavaScript -->
+      <script src="{{ asset('js/select2.min.js') }}"></script>
+      <!-- Owl Carousel JavaScript -->
+      <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+      <!-- Magnific Popup JavaScript -->
+      <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+      <!-- Smooth Scrollbar JavaScript -->
+      <script src="{{ asset('js/smooth-scrollbar.js') }}"></script>
+      <!-- lottie JavaScript -->
+      <script src="{{ asset('js/lottie.js') }}"></script>
+      <!-- am core JavaScript -->
+      <script src="{{ asset('js/core.js') }}"></script>
+      <!-- am charts JavaScript -->
+      <script src="{{ asset('js/charts.js') }}"></script>
+      <!-- am animated JavaScript -->
+      <script src="{{ asset('js/animated.js') }}"></script>
+      <!-- am kelly JavaScript -->
+      <script src="{{ asset('js/kelly.js') }}"></script>
+      <!-- am maps JavaScript -->
+      <script src="{{ asset('js/maps.js') }}"></script>
+      <!-- am worldLow JavaScript -->
+      <script src="{{ asset('js/worldLow.js') }}"></script>
+      <!-- Raphael-min JavaScript -->
+      <script src="{{ asset('js/raphael-min.js') }}"></script>
+      <!-- Morris JavaScript -->
+      <script src="{{ asset('js/morris.js') }}"></script>
+      <!-- Morris min JavaScript -->
+      <script src="{{ asset('js/morris.min.js') }}"></script>
+      <!-- Flatpicker Js -->
+      <script src="{{ asset('js/flatpickr.js') }}"></script>
+      <!-- Style Customizer -->
+      <script src="{{ asset('js/style-customizer.js') }}"></script>
+      <!-- Chart Custom JavaScript -->
+      <script src="{{ asset('js/chart-custom.js') }}"></script>
+      <!-- Custom JavaScript -->
+      <script src="{{ asset('js/custom.js') }}"></script>
+      @livewireScripts
+   </body>
+
+</html>
