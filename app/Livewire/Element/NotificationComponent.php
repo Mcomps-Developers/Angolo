57b5692dd56c01a211c7563a915f8f552b1b8df1
AlmeidaManzolo <mcompsnetwork@gmail.com>
2024-03-26 00:18:16 +0300

    Show notifications

--- app/Livewire/Element/NotificationComponent.php
+++ app/Livewire/Element/NotificationComponent.php
@@ -8,6 +8,22 @@ class NotificationComponent extends Component
 {
     public function render()
     {
-        return view('livewire.element.notification-component');
+        $user = auth()->user();
+        $notifications = $user->notifications;
+        return view('livewire.element.notification-component',['notifications'=>$notifications]);
     }
+
+    public function markNotificationAsRead($notificationId)
+    {
+        auth()->user()->notifications()->where('id', $notificationId)->update(['read_at' => now()]);
+    }
+    public function markAllNotificationsAsRead()
+    {
+        auth()->user()->unreadNotifications->markAsRead();
+    }
+    public function deleteNotification($notificationId)
+    {
+        auth()->user()->notifications()->where('id', $notificationId)->delete();
+    }
+}
 }
--- resources/views/livewire/element/notification-component.blade.php
+++ resources/views/livewire/element/notification-component.blade.php
@@ -3,60 +3,26 @@
         <div class="p-0 iq-card-body">
             <div class="p-3 bg-primary">
                 <h5 class="mb-0 text-white">All Notifications<small
-                        class="float-right pt-1 badge badge-light">4</small></h5>
+                        class="float-right pt-1 badge badge-light">{{ $notifications->count() }}</small></h5>
             </div>
-            <a href="#" class="iq-sub-card">
-                <div class="media align-items-center">
-                    <div class="">
-                        <img class="rounded avatar-40"
-                            src="{{ asset('images/user/01.jpg') }}" alt="">
-                    </div>
-                    <div class="ml-3 media-body">
-                        <h6 class="mb-0 ">Emma Watson Barry</h6>
-                        <small class="float-right font-size-12">Just Now</small>
-                        <p class="mb-0">95 MB</p>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="iq-sub-card">
-                <div class="media align-items-center">
-                    <div class="">
-                        <img class="rounded avatar-40"
-                            src="{{ asset('images/user/02.jpg') }}" alt="">
-                    </div>
-                    <div class="ml-3 media-body">
-                        <h6 class="mb-0 ">New customer is join</h6>
-                        <small class="float-right font-size-12">5 days ago</small>
-                        <p class="mb-0">Cyst Barry</p>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="iq-sub-card">
-                <div class="media align-items-center">
-                    <div class="">
-                        <img class="rounded avatar-40"
-                            src="{{ asset('images/user/03.jpg') }}" alt="">
-                    </div>
-                    <div class="ml-3 media-body">
-                        <h6 class="mb-0 ">Two customer is left</h6>
-                        <small class="float-right font-size-12">2 days ago</small>
-                        <p class="mb-0">Cyst Barry</p>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="iq-sub-card">
-                <div class="media align-items-center">
-                    <div class="">
-                        <img class="rounded avatar-40"
-                            src="{{ asset('images/user/04.jpg') }}" alt="">
-                    </div>
-                    <div class="ml-3 media-body">
-                        <h6 class="mb-0 ">New Mail from Fenny</h6>
-                        <small class="float-right font-size-12">3 days ago</small>
-                        <p class="mb-0">Cyst Barry</p>
-                    </div>
-                </div>
-            </a>
+            @if ($notifications->count() > 0)
+                @foreach ($notifications as $notification)
+                    <a href="#!" class="iq-sub-card" wire:click="markNotificationAsRead('{{ $notification->id }}')">
+                        <div class="media align-items-center">
+                            <div class="">
+                                <i class="ri-notification-2-line"></i>
+                            </div>
+                            <div class="ml-3 media-body">
+                                <h6 class="mb-0 ">{{ $notification->data['title'] }}</h6>
+                                <small
+                                    class="float-right font-size-12">{{ \Carbon\Carbon::parse($notification->created_at)->diffForHumans() }}</small>
+                                <p class="mb-0">{{ $notification->data['message'] }}</p>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+                <p>You've no notifications!</p>
+            @endif
         </div>
     </div>
 </div>
