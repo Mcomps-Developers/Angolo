b5285c1df8ebaab186c3864ff1cfb00aaa40557c
AlmeidaManzolo <mcompsnetwork@gmail.com>
2024-03-27 03:35:34 +0300

    Comment out my purchases and trending

--- app/Livewire/User/MyPurchases.php
+++ app/Livewire/User/MyPurchases.php
@@ -14,7 +14,7 @@ class MyPurchases extends Component
     use WithPagination;
     public function render()
     {
-        $myPurchases = Purchase::whereMonth('created_at', Carbon::now()->month)->where('user_id', Auth::user()->id)->where('status', 'paid')->get();
+        $myPurchases = Purchase::whereMonth('created_at', Carbon::now()->month)->where('user_id', Auth::user()->id)->where('status', 'paid')->paginate();
         $purchases = Purchase::whereMonth('created_at', Carbon::now()->month)->where('user_id', Auth::user()->id)->where('status', 'paid')->sum('amount');
         $wallet = Wallet::where('user_id', Auth::user()->id)->first();
         return view('livewire.user.my-purchases', ['myPurchases' => $myPurchases, 'wallet' => $wallet, 'purchases' => $purchases])->layout('layouts.base');
