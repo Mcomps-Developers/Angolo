6ce66fe0dba241c90c711ee1ea1a5a252a4edf8c
petermuremi351 <petermuremi351@gmail.com>
2024-05-08 13:00:00 +0300

    Update home-component.blade.php

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -9,34 +9,35 @@
 
                     <div class="col-lg-12">
                         <div class="iq-card iq-card-block iq-card-stretch iq-card-height">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                @foreach ($sliders as $key => $item)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}"
-                                        class="{{ $key == 0 ? 'active' : '' }}"></li>
-                                @endforeach
-                            </ol>
-                            <div class="carousel-inner">
-                                @foreach ($sliders as $key => $item)
-                                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                                        <a href="#!"><img src="{{ asset('images/sliders') }}/{{ $item->image }}"
-                                                class="d-block w-100" alt="#"></a>
-                                    </div>
-                                @endforeach
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    @foreach ($sliders as $key => $item)
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}"
+                                            class="{{ $key == 0 ? 'active' : '' }}"></li>
+                                    @endforeach
+                                </ol>
+                                <div class="carousel-inner">
+                                    @foreach ($sliders as $key => $item)
+                                        <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+                                            <a href="#!"><img
+                                                    src="{{ asset('images/sliders') }}/{{ $item->image }}"
+                                                    class="d-block w-100" alt="#"></a>
+                                        </div>
+                                    @endforeach
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                                    data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                                    data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
-                                data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-                                data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
                         </div>
                     </div>
-                </div>
                 @endif
                 @if ($categories->count() > 0)
                     <div class="col-lg-12">
