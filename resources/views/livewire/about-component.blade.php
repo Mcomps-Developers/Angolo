f3923234b952e3ef1f9d0af8b9faa66c9a21e21d
petermuremi351 <petermuremi351@gmail.com>
2024-05-16 08:37:37 +0300

    Fix About page

--- resources/views/livewire/about-component.blade.php
+++ resources/views/livewire/about-component.blade.php
@@ -3,7 +3,28 @@
         About us
     @endsection
     @if ($aboutContent->about_content)
-        {!! $aboutContent->about_content !!}
+    <div class="wrapper">
+        <!-- Page Content  -->
+        <div id="content-page" class="content-page">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="iq-card">
+                            <div class="iq-card-header d-flex justify-content-between">
+                                <div class="iq-header-title">
+                                    <h4 class="card-title">About {{config('app.name') }}</h4>
+                                </div>
+                            </div>
+                            <div class="iq-card-body">
+                                {!! $aboutContent->about_content !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     @else
         <div class="wrapper">
             <!-- Page Content  -->
